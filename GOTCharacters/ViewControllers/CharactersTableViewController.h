4fa766c333bf3d3b1751608daf16ac7f4fd49e7e
Patryk <patryk@Patryks-MacBook-Pro.local>
2016-08-17 20:54:08 +0200

    added CharactersTableVC, removed old VC files

---
+++ GOTCharacters/ViewControllers/CharactersTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CharactersTableViewController.h
+//  GOTCharacters
+//
+//  Created by Patryk on 17/08/16.
+//  Copyright © 2016 Patryk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CharactersTableViewController : UITableViewController
+
+@end
